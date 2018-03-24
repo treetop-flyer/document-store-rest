9f571c84fe5d71d807241093a1de123ae763db08
Martin Swiderski <martinswiderski@gmail.com>
2018-03-24 09:40:59 +0000

    trim() unit test

---
+++ tests/specs/trim-spec.js
@@ -0,0 +1,22 @@
+require('./../../src/sign-test')(__filename);
+
+var trim = require('./../../src/trim');
+
+describe('trim', function () {
+
+    it('removes leading fluff', function () {
+        expect(trim('     agshsd')).toBe('agshsd');
+        expect(trim('\t     agshsd')).toBe('agshsd');
+        expect(trim('\n\r     agshsd')).toBe('agshsd');
+    });
+    it('removes trailing fluff', function () {
+        expect(trim('agshsd     ')).toBe('agshsd');
+        expect(trim('agshsd\t     ')).toBe('agshsd');
+        expect(trim('agshsd\n\r     ')).toBe('agshsd');
+    });
+    it('removes fluff both ends', function () {
+        expect(trim('   agshsd  ')).toBe('agshsd');
+        expect(trim('\t    agshsd\t     ')).toBe('agshsd');
+        expect(trim('\n      agshsd\n\r     ')).toBe('agshsd');
+    });
+});
